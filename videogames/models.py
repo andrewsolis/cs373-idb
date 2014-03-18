5a75227d253e509a3a9405784449271ad7817825
poultergiest <ethan.poulter@utexas.edu>
2014-03-18 18:29:13 -0500

    almost don with django models

--- videogames/models.py
+++ videogames/models.py
@@ -1,35 +1,68 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Images(models.Model):
+	#id, link 
+	link = models.URLField()
+	
+
+class Videos(models.Model):
+	#id, link 
+	link = models.URLField()
+
+class Genre(model.Model):
+	#id, #types
+	types = models.CharField(max_length=100)
+
+class Job(models.Model):
+	#id, #profession
+	profession = models.CharField(max_length=100);
+
+class System(models.Model):
+	#id, #platform
+	platform = models.CharField(max_length=100);
+
 
 class Game(models.Model):
 	# name, id, system, release_date, genre, publisher, 
 	# developer, synopsis, copies_sold, images, videos, People, Companies.
 	name = models.CharField(max_length=100)
-	system = models.CharField(max_length=100)
-	genre = models.CharField(max_length=100)
+	system = models.ForeignKey(System)
+	genre = models.ManyToManyField(Genre)
 	synopsis = models.CharField(max_length=1000)
 	copies = models.IntegerField(default=0)
 	release_date = models.DateTimeField('date published')
-	companies = models.ForeignKey(Company)
-	people = models.ForeignKey(Person)
-
-class Company(models.Model):
-	# name, id, founded, location, kind, description, 
-	# images, maps, external_links, contact_info, Games. 
-	name = models.CharField(max_length=100)
-	founded = models.DateTimeField('date founded')
-	location = models.CharField(max_length=100)
-	kind = models.CharField(max_length=100)
-	games = models.ForeignKey(Game)
-	people = models.ForeignKey(Person)
-
+	company = models.ForeignKey(Company)
+	people = models.ManyToManyField(Person)
+	images = models.
+	videos = models.
+	gamefaq = models.URLField()
 
 class Person(models.Model):
 	# name, id, DOB, location, job, description, images, Games, Companies
 	name = models.CharField(max_length=100)
 	DOB = models.DateTimeField('date born')
 	title = models.CharField(max_length=100)
+	jobs = models.ManyToMany(Job)
+	description = models.CharField(max_length=1000)
 	residence = models.CharField(max_length=100)
-	games = models.ForeignKey(Game)
-	companies = models.ForeignKey(Company)
+	# games = models.ManyToMany(Game)
+	companies = models.ManyToMany(Company)
+	images = models.
+	videos = models.
+
+class Company(models.Model):
+	# name, id, founded, location, kind, description, 
+	# images, maps, external_links, contact_info, Games. 
+	name = models.CharField(max_length=100)
+	founded = models.DateTimeField('date founded')
+	description = models.CharField(max_length=1000)
+	location = models.CharField(max_length=100)
+	kind = models.CharField(max_length=100)
+	# games = models.ManyToMany(Game)
+	# people = models.ManyToMany(Person)
+	images = models.
+	_map = models.URLField()
+	webpage = models.URLField()
+
+
