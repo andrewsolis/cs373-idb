884d11d295b70c4103aebaed4a79b087245174a2
poultergiest <ethan.poulter@utexas.edu>
2014-03-18 20:13:02 -0500

    finished models?

--- videogames/models.py
+++ videogames/models.py
@@ -1,32 +1,36 @@
 # Create your models here.
 from django.db import models
 
+class Media(models.Model):
+	link = models.URLField()
+	other_id = models.IntegerField()
+	other_type = models.CharField(choices=[('GM', 'Game'), ('PPL', 'People'), ('CP', 'Company')])
+
 class Images(models.Model):
 	#id, link 
-	link = models.URLField()
-	
+	pass
 
 class Videos(models.Model):
 	#id, link 
-	link = models.URLField()
+	pass
 
 class Genre(model.Model):
 	#id, #types
-	types = models.CharField(max_length=100)
+	types = models.CharField(max_length=25)
 
 class Job(models.Model):
 	#id, #profession
-	profession = models.CharField(max_length=100);
+	profession = models.CharField(max_length=25);
 
 class System(models.Model):
 	#id, #platform
-	platform = models.CharField(max_length=100);
+	platform = models.CharField(max_length=25);
 
 
 class Game(models.Model):
 	# name, id, system, release_date, genre, publisher, 
 	# developer, synopsis, copies_sold, images, videos, People, Companies.
-	name = models.CharField(max_length=100)
+	name = models.CharField(max_length=25)
 	system = models.ForeignKey(System)
 	genre = models.ManyToManyField(Genre)
 	synopsis = models.CharField(max_length=1000)
@@ -34,35 +38,51 @@ class Game(models.Model):
 	release_date = models.DateTimeField('date published')
 	company = models.ForeignKey(Company)
 	people = models.ManyToManyField(Person)
-	images = models.
-	videos = models.
 	gamefaq = models.URLField()
+
+	def images(self):
+		results = Images(other_id = self.id, other_type = 'GM')
+		return results.objects
+
+	def videos(self):
+		results = Videos(other_id = self.id, other_type = 'GM')
+		return results.objects
 
 class Person(models.Model):
 	# name, id, DOB, location, job, description, images, Games, Companies
-	name = models.CharField(max_length=100)
+	name = models.CharField(max_length=25)
 	DOB = models.DateTimeField('date born')
-	title = models.CharField(max_length=100)
+	title = models.CharField(max_length=25)
 	jobs = models.ManyToMany(Job)
 	description = models.CharField(max_length=1000)
-	residence = models.CharField(max_length=100)
-	# games = models.ManyToMany(Game)
+	residence = models.CharField(max_length=50)
 	companies = models.ManyToMany(Company)
-	images = models.
-	videos = models.
+	# games = models.ManyToMany(Game)
+	
+	def images(self):
+		results = Images(other_id = self.id, other_type = 'PPL')
+		return results.objects
+
+	def videos(self):
+		results = Videos(other_id = self.id, other_type = 'PPL')
+		return results.objects
 
 class Company(models.Model):
 	# name, id, founded, location, kind, description, 
 	# images, maps, external_links, contact_info, Games. 
-	name = models.CharField(max_length=100)
+	name = models.CharField(max_length=25)
 	founded = models.DateTimeField('date founded')
 	description = models.CharField(max_length=1000)
-	location = models.CharField(max_length=100)
-	kind = models.CharField(max_length=100)
+	location = models.CharField(max_length=50)
+	mapimage = models.URLField()
+	webpage = models.URLField()
 	# games = models.ManyToMany(Game)
 	# people = models.ManyToMany(Person)
-	images = models.
-	_map = models.URLField()
-	webpage = models.URLField()
 
+	def images(self):
+		results = Images(other_id = self.id, other_type = 'CP')
+		return results.objects
+
+	def videos(self):
+		return QuerySet.none().objects
 
