4b4f4d4d2602145c5ce9f1826335e4811133a84d
poultergiest <ethan.poulter@utexas.edu>
2014-03-19 19:24:41 -0500

    more fixes

--- idb/urls.py
+++ idb/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url('^home/', 'idb.views.home', name='home'),
     url('^$', 'idb.views.home', name='home'),
     url('^metroid/', 'idb.views.metroid', name='metroid'),
-    url('^crash bandicoot/', 'idb.views.crash bandicoot', name='crash bandicoot')
+    url('^crash_bandicoot/', 'idb.views.crash_bandicoot', name='crash_bandicoot')
     url('^nintendo/', 'idb.views.nintendo', name='nintendo'),
 
 )
