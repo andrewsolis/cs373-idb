d191dd2bdf18df74af8bad233ed49f3abb84db4c
Mitch Stephan <mjs4373@cs.utexas.edu>
2014-03-30 15:06:12 -0500

    Modify api/view

--- idb/api/views.py
+++ idb/api/views.py
@@ -12,7 +12,7 @@ def validate_game_json(game_object) :
 def games(request):
 	games_list = []
 	if(request.method == 'GET'):
-		games_list = serializers.serialize("json", Game.objects.order_by('-id').all())
+		games_list = serializers.serialize("json", Game.objects.all())
 	elif(request.method == 'POST'):
 		request_data = requset.POST
 		for deserialized_object in serializers.deserialize("json", request_data) :
@@ -31,7 +31,6 @@ def games_id(request, game_id):
 		Game.objects.get(pk = int(game_id)).delete()
 	return HttpResponse(game, content_type = "application/json")
 
-# doesn't work 
 def games_people(request, game_id):
 	response = "empty"
 	game = Game.objects.filter(pk = int(game_id)).values('people')[0] # output: {'people': None}
@@ -44,13 +43,13 @@ def games_people(request, game_id):
 def games_companies(request, game_id):
 	companies_list = []
 	if (request.method == 'GET'):
-		companies_list = serializers.serialize("json", Company.objects.order_by('-id').all())
+		companies_list = serializers.serialize("json", Company.objects.all())
 	return HttpResponse(companies_list, content_type = "application/json")
 
 def people(request):
 	people_list = []
 	if(request.method == 'GET'):
-		people_list = serializers.serialize("json", Person.objects.order_by('-id').all())
+		people_list = serializers.serialize("json", Person.objects.all())
 	elif(request.method == 'POST'):
 		pass
 
@@ -75,7 +74,7 @@ def people_companies(request, people_id):
 def companies(request):
 	companies_list = []
 	if(request.method == 'GET'):
-		companies_list = serializers.serialize("json",Company.objects.order_by('-id').all())
+		companies_list = serializers.serialize("json",Company.objects.all())
 	elif(request.method == 'POST'):
 		pass
 
