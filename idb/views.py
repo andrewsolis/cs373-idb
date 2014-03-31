7e9e16d59a25d82dc46b51e4b13d2551ddd20831
Yousup Lee <y.lee@utexas.edu>
2014-03-31 18:29:57 -0500

    fixed the date format

--- idb/views.py
+++ idb/views.py
@@ -19,6 +19,7 @@ def games_id(request, id):
 	game = api_games_id(request,id)
 	game_content = json.loads(game.content.decode("utf-8"))
 	content = game_content[0]["fields"]
+	game_content[0]["fields"]["release_date"] = game_content[0]["fields"]["release_date"][:10]
 	return render_to_response('game.html', content)
 
 def games_people(request, id):
@@ -36,6 +37,7 @@ def people_id(request, id):
 	person = api_people_id(request,id)
 	person_content = json.loads(person.content.decode("utf-8"))
 	content = person_content[0]["fields"]
+	person_content[0]["fields"]["DOB"] = person_content[0]["DOB"][:10]
 	return render_to_response('person.html', content)
 
 def people_games(request, id):
@@ -55,6 +57,7 @@ def companies_id(request, id):
 	company = api_companies_id(request,id)
 	company_content = json.loads(company.content.decode("utf-8"))
 	content = company_content[0]["fields"]
+	company_content[0]["fields"]["founded"] = company_content[0]["founded"][:10]
 	return render_to_response('company.html', content)
 
 def companies_games(request, id):
