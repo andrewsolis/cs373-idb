6951936b2b8f3252586cae794e200a957f66f3fb
poultergiest <ethan.poulter@utexas.edu>
2014-04-15 20:53:22 -0500

    AND search working

--- idb/search.py
+++ idb/search.py
@@ -15,6 +15,7 @@ def search_crawl(request, query_string):
     companies_list = []
     people_list = []
 
+    query_string_list = query_string.split(" ")
     result_list = []
 
     base_url = 'http://retro-video-games-373.herokuapp.com/'
@@ -27,21 +28,48 @@ def search_crawl(request, query_string):
     for game in games:
         g = {}
         g['pk'] = game['pk']
-        u = urllib.request.urlopen(base_url + "games/" + str(g['pk']) + "/")
+        g['url'] = base_url + "games/" + str(g['pk']) + "/"
+        g['total'] = 0
+        u = urllib.request.urlopen(g['url'])
         soup = BeautifulSoup(u.read())
-        gfound = soup.findAll(text = re.compile(query_string, re.IGNORECASE))
-        games_list.append(gfound)
+        for s in query_string_list:
+            gfound = soup.findAll(text = re.compile(s, re.IGNORECASE))
+            g[s] = len(gfound)
+            if(g[s] > 0): 
+                g['total'] = g['total'] + 1
+        if(g['total'] > 0):
+            games_list.append(g)        
 
     for company in companies:
         c = {}
         c['pk'] = company['pk']
-        u = urllib.request.urlopen(base_url + "companies/" + str(c['pk']) + "/")
-        companies_list.append(c)
+        c['url'] = base_url + "companies/" + str(c['pk']) + "/"
+        c['total'] = 0
+        u = urllib.request.urlopen(c['url'])
+        soup = BeautifulSoup(u.read())
+        for s in query_string_list:
+            cfound = soup.findAll(text = re.compile(s, re.IGNORECASE))
+            c[s] = len(cfound)
+            if(c[s] > 0): 
+                c['total'] = c['total'] + 1
+        if(c['total'] > 0):
+            companies_list.append(c)
 
     for person in people:
         p = {}
         p['pk'] = person['pk']
-        u = urllib.request.urlopen(base_url + "people/" + str(p['pk']) + "/")
-        people_list.append(p)
+        p['url'] = base_url + "people/" + str(p['pk']) + "/"
+        p['total'] = 0
+        u = urllib.request.urlopen(p['url'])
+        soup = BeautifulSoup(u.read())
+        for s in query_string_list:
+            pfound = soup.findAll(text = re.compile(s, re.IGNORECASE))
+            p[s] = len(pfound)
+            if(p[s] > 0): 
+                p['total'] = p['total'] + 1
+        if(p['total'] > 0):
+            people_list.append(p)
 
-    return games_list
+    result_list = games_list + companies_list + people_list
+    result_list = sorted(result_list, key = lambda x: x['total'], reverse = True)
+    return result_list
